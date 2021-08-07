1ad24b96013dede89e353f4ab2fd735e83be9070
Hira <hiralalumeshkumhar@gmail.com>
2021-08-07 14:16:04 +0530

    first commit

---
+++ aboutme.php
@@ -0,0 +1,11 @@
+<?php include('nav.php'); ?>
+
+<!------ Include the above in your HEAD tag ---------->
+
+
+               <div> <img src="https://lh3.googleusercontent.com/UIHhwjPi6BEJ3rswvv1BNcFMC24KPY37ykMCcU_bV5TuvWJbEISiqd815yt-jAw3UcN7-Gfa15cEgLnDDpNit7KkKYFQEq7zWjbfigoF6AsynBGbcBRT2bwLAs70pJpaz9fmoOIOpw=w2400" alt="Me" width="100rem"/></div>
+                Hi,its Hiralal Umesh Kumhar here.I am currently pursuing my Bachelor in Engineering from Atharva College of Engineering.I have a keen interest in field of Programming and Web development.Looking forward to work in a company to enhance my skills and learn about the cooperative world.
+                
+            
+
+
---
+++ index.php
@@ -0,0 +1,20 @@
+<?php include('nav.php') ?>
+
+
+  <h1>Welcome to Bank</h1>
+
+
+  <div class="bg">
+    <div class="center">
+      <ul>
+        <li class="operations"><a href="transfer_money.php"><button class="btn" id="red">Users</button></a></li>
+        <li class="operations"><a href="transfer_money.php"><button class="btn" id="blue">Trasnfer Money</button></a></li>
+        <li class="operations"><a href="transactionhistory.php"><button class="btn" id="green">Transfer History</button></a></li>
+      </ul>
+    </div>
+  </div>
+
+  <?php include('footer.php') ?>
+  
+</body>
+</html>
